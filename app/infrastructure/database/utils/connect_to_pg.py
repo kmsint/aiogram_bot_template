e923bbc8766d9add5900975ae4442509a057f134
kmsint <kmsrus@gmail.com>
2024-11-15 11:52:45 +0300

    refactor: Wrap in a try/except database connection

--- app/infrastructure/database/utils/connect_to_pg.py
+++ app/infrastructure/database/utils/connect_to_pg.py
@@ -12,13 +12,23 @@ async def get_pg_pool(
     user: str,
     password: str,
 ) -> AsyncConnectionPool:
-    dp_pool = AsyncConnectionPool(
-        conninfo=f'postgresql://{user}:{password}@{host}:{port}/{db_name}',
-        min_size=1,
-        max_size=3,
-    )
-    # version = await db_pool.connection(). fetchone("SELECT version() as ver;")
-    # logger.info(f"Connected to {version['ver']}")
-    await dp_pool.open()
 
-    return dp_pool
+    try:
+        db_pool = AsyncConnectionPool(
+            conninfo=f'postgresql://{user}:{password}@{host}:{port}/{db_name}',
+            min_size=1,
+            max_size=3,
+            open=False
+        )
+        
+        await db_pool.open()
+
+        async with db_pool.connection() as connection:
+            async with connection.cursor() as cursor:
+                await cursor.execute("SELECT version();")
+                db_version = await cursor.fetchone()
+                logger.info(f"Connected to {db_version[0]}")
+        return db_pool
+    except Exception as e:
+        logger.exception('Something went wrong while connecting to the database with exception: %s', e)
+        raise
