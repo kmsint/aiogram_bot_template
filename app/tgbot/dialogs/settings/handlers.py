fc9b49b3463db7efb490aa76587d8052d6bc2b1a
kmsint <kmsrus@gmail.com>
2025-01-27 21:01:02 +0300

    bugfix: Fix i18n little bug

--- app/tgbot/dialogs/settings/handlers.py
+++ app/tgbot/dialogs/settings/handlers.py
@@ -33,6 +33,7 @@ async def update_user_lang(
     radio_lang: ManagedRadio = dialog_manager.find("radio_lang")
     checked_locale = locales[int(radio_lang.get_checked()) - 1]
     i18n: TranslatorRunner = translator_hub.get_translator_by_locale(checked_locale)
+    dialog_manager.middleware_data['i18n'] = i18n
 
     await db.users.update_user_lang(user_id=callback.from_user.id, user_lang=checked_locale)
     await callback.answer(text=i18n.lang.saved())
