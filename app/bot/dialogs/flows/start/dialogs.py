af01eea163caaa081140f8705d26f6696c1c1d08
kmsint <kmsrus@gmail.com>
2025-06-04 18:52:39 +0300

    feature: Add custom dialog widget I18nFormat

--- app/bot/dialogs/flows/start/dialogs.py
+++ app/bot/dialogs/flows/start/dialogs.py
@@ -3,7 +3,13 @@ from aiogram_dialog.widgets.text import Format
 
 from app.bot.dialogs.flows.start.getters import get_hello
 from app.bot.dialogs.flows.start.states import StartSG
+from app.bot.dialogs.widgets.i18n import I18nFormat
 
 start_dialog = Dialog(
-    Window(Format("{hello}"), getter=get_hello, state=StartSG.start),
+    Window(
+        Format("{hello}"),
+        I18nFormat("bot-desription"),
+        getter=get_hello, 
+        state=StartSG.start
+    ),
 )
--- app/bot/dialogs/widgets/i18n.py
+++ app/bot/dialogs/widgets/i18n.py
@@ -1,17 +1,30 @@
+import logging
+
 from aiogram_dialog.api.protocols import DialogManager
 from aiogram_dialog.widgets.common import WhenCondition
 from aiogram_dialog.widgets.text import Text
 from fluentogram import TranslatorRunner
 
+logger = logging.getLogger(__name__)
+
 
 class I18nFormat(Text):
-    def __init__(self, key: str, when: WhenCondition = None):
+    def __init__(self, ftl_key: str, when: WhenCondition = None):
         super().__init__(when)
-        self.key = key
+        self.ftl_key = ftl_key
 
     async def _render_text(self, data: dict, dialog_manager: DialogManager) -> str:
+        
         i18n: TranslatorRunner = dialog_manager.middleware_data.get("i18n")
-        value = i18n.get(self.key, **data)
+        
+        if i18n is None:
+            logger.error("TranslatorRunner object is not provided in middleware data.")
+            raise RuntimeError("Missing `i18n` in middleware context.")
+        
+        value = i18n.get(self.ftl_key, **data)
+
         if value is None:
-            raise KeyError(f'Translation key="{self.key}" not found')
+            logger.error("Translation ftl_key='%s' was not found.", self.ftl_key)
+            raise KeyError(f'Translation ftl_key="{self.ftl_key}" not found')
+        
         return value
